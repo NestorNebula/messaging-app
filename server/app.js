f258947612d7c8d4dc7070e79ff3c5531c77fe84
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-24 14:32:44 +0200

    feat: add error handling with Sperror

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const routes = require('./routes/routes');
+const Sperror = require('sperror');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,22 @@ app.use('/users', routes.user);
 app.use('/profiles', routes.profile);
 app.use('/messages', routes.message);
 app.use('/chats', routes.chat);
+
+app.use((req, res, next) => {
+  next(new Sperror('Not Found', "The resource couln't be found.", 404));
+});
+
+app.use((err, req, res, next) => {
+  err instanceof Sperror
+    ? res.json({ error: err })
+    : res.json({
+        error: new Sperror(
+          'Server error',
+          'The app faced an unexpected error.',
+          500
+        ),
+      });
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App listening on Port ${process.env.PORT}`)
