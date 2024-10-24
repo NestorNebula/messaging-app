5a77f651a383ff9146ef4ddea7e6e68b89d45217
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-24 14:23:33 +0200

    feat: add routes in app.js file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const routes = require('./routes/routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', routes.auth);
+app.use('/users', routes.user);
+app.use('/profiles', routes.profile);
+app.use('/messages', routes.message);
+app.use('/chats', routes.chat);
 
 app.listen(process.env.PORT, () =>
   console.log(`App listening on Port ${process.env.PORT}`)
