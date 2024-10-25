ef0a0fe48c8c95a09ed244b30e12afa7f6e324fb
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-25 11:17:41 +0200

    test: add first test for auth route & signup

---
+++ server/tests/auth.test.js
@@ -0,0 +1,28 @@
+const { request, app } = require('./setup');
+const router = require('../routes/auth');
+const { getFakeUser } = require('../helpers/faker');
+
+app.use('/', router);
+
+jest.mock('../models/queries', () => {
+  return {
+    createUser: (user) => {
+      return user;
+    },
+  };
+});
+
+describe('POST signup', () => {
+  it('returns 201 code after creating user', (done) => {
+    const user = getFakeUser();
+    request(app)
+      .post('/signup')
+      .send({
+        username: user.username,
+        email: user.email,
+        password: user.password,
+      })
+      .type('form')
+      .expect(201, done());
+  });
+});
