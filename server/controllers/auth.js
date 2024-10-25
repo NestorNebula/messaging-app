d9b394eb393269b5ac2f749da1f507c6e4c9af39
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-25 10:26:05 +0200

    feat: add signUp function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,33 @@
-const signUp = () => {};
+const { validationResult } = require('express-validator');
+const { validateUser } = require('../helpers/validation');
+const bcrypt = require('bcrypt');
+const Sperror = require('sperror');
+const prisma = require('../models/queries');
+
+const signUp = [
+  validateUser,
+  (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        throw new Sperror('Server error', 'Error during password hash.', 500);
+      }
+      const user = await prisma.createUser({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: 'default.png',
+      });
+      if (!user) {
+        throw new Sperror('Server error', 'Error when creating user.', 500);
+      }
+      res.sendStatus(201);
+    });
+  },
+];
 
 const logIn = () => {};
 
