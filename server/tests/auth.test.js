f4525f963fb3c5d5c8f78cbe92308b4bd11cafa4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-25 13:57:42 +0200

    test: add tests for login

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,8 +1,13 @@
 const { request, app } = require('./setup');
 const router = require('../routes/auth');
 const { getFakeUser } = require('../helpers/faker');
+const bcrypt = require('bcrypt');
 
 app.use('/', router);
+
+const user = getFakeUser();
+const mockUser = user;
+const mockHash = bcrypt.hashSync(user.password, 10);
 
 jest.mock('../models/queries', () => {
   return {
@@ -15,12 +20,17 @@ jest.mock('../models/queries', () => {
     getUserByEmail: () => {
       return null;
     },
+    getUserByUsermail: () => {
+      return {
+        ...mockUser,
+        password: mockHash,
+      };
+    },
   };
 });
 
 describe('POST signup', () => {
   it('returns 201 code after creating user', (done) => {
-    const user = getFakeUser();
     request(app)
       .post('/signup')
       .send({
@@ -38,5 +48,28 @@ describe('POST signup', () => {
       .send({ username: 'username', email: 'email@email.com', password: 'pwd' })
       .type('form')
       .expect(400, done);
+  });
+});
+
+describe('POST login', () => {
+  it('returns cookies when logging in', () => {
+    return request(app)
+      .post('/login')
+      .send({ username: user.username, password: user.password })
+      .type('form')
+      .expect(200)
+      .then((res) => {
+        expect(res.headers['set-cookie'][1]).not.toBeNull();
+      });
+  });
+
+  it('returns error when users informations are invalid', () => {
+    return request(app)
+      .post('/login')
+      .send({ username: user.username, password: mockHash })
+      .type('form')
+      .then((res) => {
+        expect(res.statusCode).toBe(400);
+      });
   });
 });
