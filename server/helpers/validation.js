c9f19318662c52e7283b5ad3e2aa102cec5ca647
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-25 10:12:41 +0200

    feat: update validateUser

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -5,6 +5,7 @@ const validateUser = [
   body('username')
     .trim()
     .blacklist('<>')
+    .notEmpty()
     .custom(async (username) => {
       const existingUser = await prisma.getUserByUsername(username);
       if (existingUser) throw new Error('Username already taken.');
@@ -12,10 +13,16 @@ const validateUser = [
   body('email')
     .trim()
     .blacklist('<>')
+    .notEmpty()
     .custom(async (email) => {
       const existingEmail = await prisma.getUserByEmail(email);
       if (existingEmail) throw new Error('Email already taken.');
     }),
+  body('password')
+    .trim()
+    .notEmpty()
+    .isLength({ min: 8, max: 30 })
+    .withMessage('Username must have between 8 and 30 characters.'),
 ];
 
 module.exports = { validateUser };
