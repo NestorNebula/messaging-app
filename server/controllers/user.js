6cb3734d9a3d2831ef56a3f33937b8dc60367052
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-26 09:09:19 +0200

    feat: add getUser function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,17 @@
-const getUser = () => {};
+const prisma = require('../models/queries');
+const Sperror = require('sperror');
+
+const getUser = async (req, res, next) => {
+  const requestedUserId = +req.params.userId;
+  if (req.user.id !== requestedUserId) {
+    return next(new Sperror('Forbidden', "You can't access this data.", 403));
+  }
+  const user = await prisma.getUser(requestedUserId);
+  if (!user) {
+    return next(new Sperror('Not Found', "The user couldn't be found.", 404));
+  }
+  res.json({ user });
+};
 
 const putUser = () => {};
 
