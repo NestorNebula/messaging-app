230b0865b73701ba67387cbb3b608bd5f32bc694
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-26 09:07:21 +0200

    test: set req.user value before tests

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -2,10 +2,14 @@ const { request, app } = require('./setup');
 const router = require('../routes/user');
 const { getFakeUser } = require('../helpers/faker');
 
-app.use('/', router);
-
 const user = getFakeUser();
 const mockUser = user;
+
+app.use('/', (req, res, next) => {
+  req.user = user;
+  next();
+});
+app.use('/', router);
 
 jest.mock('../models/queries', () => {
   return {
@@ -19,7 +23,6 @@ describe('GET user', () => {
   it('returns user', () => {
     return request(app)
       .get(`/${user.id}`)
-      .send({ user })
       .then((res) => {
         expect(res.body.user).toEqual(user);
       });
@@ -28,7 +31,6 @@ describe('GET user', () => {
   it('returns 403 when trying to get another user', (done) => {
     request(app)
       .get(`/${user.id + 1}`)
-      .send({ user })
       .expect(403, done);
   });
 });
