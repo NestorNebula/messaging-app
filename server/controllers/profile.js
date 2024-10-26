bc44a907d074dde8462b2f47e632e13cfa468096
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-26 16:20:16 +0200

    feat: add putProfile function

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,5 +1,7 @@
 const prisma = require('../models/queries');
 const Sperror = require('sperror');
+const { validationResult } = require('express-validator');
+const { validateProfile } = require('../helpers/validation');
 
 const getProfile = async (req, res, next) => {
   const profile = await prisma.getProfile(+req.params.userId);
@@ -20,6 +22,30 @@ const getProfiles = async (req, res, next) => {
   res.json({ profiles });
 };
 
-const putProfile = () => {};
+const putProfile = [
+  validateProfile,
+  async (req, res, next) => {
+    const userIdProfileToUpdate = +req.params.userId;
+    if (req.user.id !== userIdProfileToUpdate) {
+      return next(new Sperror('Forbidden', "You can't update this data.", 403));
+    }
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const updatedProfile = await prisma.updateProfile(userIdProfileToUpdate, {
+      displayName: req.body.displayName,
+      avatar: req.body.avatar,
+      bio: req.body.bio,
+      link: req.body.link,
+    });
+    if (!updatedProfile) {
+      return next(
+        new Sperror('Server Error', 'Error when updating profile.', 500)
+      );
+    }
+    res.json({ profile: updatedProfile });
+  },
+];
 
 module.exports = { getProfile, getProfiles, putProfile };
