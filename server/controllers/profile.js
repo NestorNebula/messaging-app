025f51a7cb3a9249240172afb937d0d027257ff9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-26 14:48:20 +0200

    feat: add getProfile function

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,4 +1,13 @@
-const getProfile = () => {};
+const prisma = require('../models/queries');
+const Sperror = require('sperror');
+
+const getProfile = async (req, res, next) => {
+  const profile = await prisma.getProfile(+req.params.userId);
+  if (!profile) {
+    return next(new Sperror('Not Found', "Profile couldn't be found.", 404));
+  }
+  res.json({ profile });
+};
 
 const getProfiles = () => {};
 
