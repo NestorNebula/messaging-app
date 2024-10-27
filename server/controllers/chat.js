00c43dda3ca22067529e9c867c8ec27801773401
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-27 08:24:42 +0100

    feat: add getChats function

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,4 +1,17 @@
-const getChats = () => {};
+const prisma = require('../models/queries');
+const Sperror = require('sperror');
+
+const getChats = async (req, res, next) => {
+  const userId = +req.params.userId;
+  if (req.user.id !== userId) {
+    return next(new Sperror('Forbidden', "You can't acces this data.", 404));
+  }
+  const chats = await prisma.getUserChats();
+  if (!chats) {
+    return next(new Sperror('Server Error', 'Error when fetching chats.', 500));
+  }
+  res.json({ chats });
+};
 
 const postChat = () => {};
 
