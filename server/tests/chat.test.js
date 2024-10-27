8e023901d038023d87bf5e3feda0069e5fca0a78
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-27 08:07:51 +0100

    test: add tests for get user chats

--- server/tests/chat.test.js
+++ server/tests/chat.test.js
@@ -1,9 +1,12 @@
 const { request, app } = require('./setup');
 const router = require('../routes/chat');
 const userRouter = require('../routes/user');
-const { getFakeUser } = require('../helpers/faker');
+const { getFakeUser, getFakeChat } = require('../helpers/faker');
 
 const user = getFakeUser();
+const mockUser = user;
+const mockChats = [getFakeChat(user), getFakeChat(), getFakeChat()];
+
 app.use('/', (req, res, next) => {
   req.user = user;
   next();
@@ -15,7 +18,11 @@ describe('GET chats', () => {
     return request(app)
       .get(`/${user.id}/chats`)
       .then((res) => {
-        // TODO: expect chats to match fakeChats
+        expect(res.body.chats.length).toBe(1);
       });
+  });
+
+  it("returns 403 when trying to access other user's chats", (done) => {
+    request(app).get(`/${mockChats[1].users[0].id}`).expect(403, done);
   });
 });
