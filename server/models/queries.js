cdb52d86f23707d2712b0b3b6e48302b01dd2942
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-27 08:18:48 +0100

    feat: udpate getUserChats query
    Avoid including informations like password

--- server/models/queries.js
+++ server/models/queries.js
@@ -139,7 +139,10 @@ const getUserChats = async (userId) => {
     include: {
       messages: true,
       users: {
-        include: {
+        select: {
+          id: true,
+          username: true,
+          online: true,
           profile: true,
         },
       },
