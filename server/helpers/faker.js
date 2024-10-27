55d9ac9522e0f589ce3a364d5cbe5ee6c22957c3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-27 16:55:26 +0100

    feat: add getFakeMessage helper function

--- server/helpers/faker.js
+++ server/helpers/faker.js
@@ -33,7 +33,25 @@ const getFakeChat = (user) => {
     creationDate: new Date(Date.now()),
     updatedAt: new Date(Date.now()),
     users: user ? [user, getFakeUser()] : [getFakeUser(), getFakeUser()],
+    messages: [],
   };
 };
 
-module.exports = { getFakeUser, getFakeProfile, getFakeFriend, getFakeChat };
+const getFakeMessage = ({ content, file }) => {
+  return {
+    id: faker.number.int({ max: 10000 }),
+    content: content ?? faker.lorem.paragraph(),
+    file: file ?? faker.string.binary(),
+    creationDate: new Date(Date.now()),
+    userId: faker.number.int({ max: 100 }),
+    chatId: faker.number.int({ max: 1000 }),
+  };
+};
+
+module.exports = {
+  getFakeUser,
+  getFakeProfile,
+  getFakeFriend,
+  getFakeChat,
+  getFakeMessage,
+};
