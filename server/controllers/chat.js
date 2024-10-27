4a9dbbe7e5fdc4b378a05ac1a9ede12c8bceb60b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-27 11:20:38 +0100

    feat: add putChat function

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -26,9 +26,29 @@ const postChat = async (req, res, next) => {
   }
   usersArray.unshift(req.user.id);
   const chat = await prisma.createChat(usersArray);
+  if (!chat) {
+    return next(new Sperror('Server Error', 'Error when creating chat.', 500));
+  }
   res.status(201).json({ chat });
 };
 
-const putChat = () => {};
+const putChat = async (req, res, next) => {
+  const usersArray = JSON.parse(req.body.users);
+  if (!Array.isArray(usersArray)) {
+    return next(
+      new Sperror(
+        'Bad Request',
+        'No user list provided for chat creation.',
+        400
+      )
+    );
+  }
+  const chatId = +req.params.chatId;
+  const updatedChat = await prisma.updateChat(chatId, usersArray);
+  if (!updatedChat) {
+    return next(new Sperror('Server Error', 'Error when updating chat.', 500));
+  }
+  res.json({ chat: updatedChat });
+};
 
 module.exports = { getChats, postChat, putChat };
