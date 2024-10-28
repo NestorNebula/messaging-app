195307ba8a6c6a6cc47bc7c14ba5a77806f500c6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-28 11:08:55 +0100

    feat: add deleteMessage function

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -46,6 +46,22 @@ const putMessage = async (req, res, next) => {
   res.json({ message: updatedMessage });
 };
 
-const deleteMessage = () => {};
+const deleteMessage = async (req, res, next) => {
+  const message = await prisma.getMessage(+req.params.messageId);
+  if (!message) {
+    return next(
+      new Sperror(
+        'Not Found',
+        "The message you are trying to delete doesn't exist.",
+        404
+      )
+    );
+  }
+  if (message.userId !== req.user.id) {
+    return next(new Sperror('Forbidden', "You can't delete this data.", 403));
+  }
+  await prisma.deleteMessage(message.id);
+  res.sendStatus(200);
+};
 
 module.exports = { postMessage, putMessage, deleteMessage };
