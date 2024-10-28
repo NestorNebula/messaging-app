542bf42f3832978a5ad72c07d6e665c4b968fa21
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-28 08:38:25 +0100

    test: add tests for post message

--- server/tests/message.test.js
+++ server/tests/message.test.js
@@ -1,15 +1,36 @@
 const { request, app } = require('./setup');
 const router = require('../routes/message');
 const chatRouter = require('../routes/chat');
-const { getFakeUser } = require('../helpers/faker');
+const { getFakeUser, getFakeChat } = require('../helpers/faker');
 
 const user = getFakeUser();
+const chat = getFakeChat();
 app.use('/', (req, res, next) => {
   req.user = user;
   next();
 });
 
-describe.skip('POST message', () => {
-  app.use('/', chatRouter);
-  it("doesn't do anything for now", () => {});
+describe('POST message', () => {
+  it('returns created message', () => {
+    app.use('/', chatRouter);
+    return request(app)
+      .post(`/${chat.id}/messages`)
+      .send({ content: 'This is my message.' })
+      .type('form')
+      .expect(201)
+      .then((res) => {
+        expect(res.body.message.chatId).toBe(chat.id);
+        expect(res.body.message.userId).toBe(user.id);
+        expect(res.body.message.content).toBe('This is my message.');
+      });
+  });
+
+  it('returns 400 if no content/file is provided', (done) => {
+    app.use('/', chatRouter);
+    request(app)
+      .post(`/${chat.id}/messages`)
+      .send({})
+      .type('form')
+      .expect(400, done);
+  });
 });
