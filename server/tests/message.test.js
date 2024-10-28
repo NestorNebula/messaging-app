fcd87e60adebea80453feb1d0d8a46f108c35261
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-28 09:43:20 +0100

    test: add mocks for queries

--- server/tests/message.test.js
+++ server/tests/message.test.js
@@ -1,16 +1,35 @@
 const { request, app } = require('./setup');
 const router = require('../routes/message');
 const chatRouter = require('../routes/chat');
-const { getFakeUser, getFakeChat } = require('../helpers/faker');
+const {
+  getFakeUser,
+  getFakeChat,
+  getFakeMessage,
+} = require('../helpers/faker');
 
 const user = getFakeUser();
-const chat = getFakeChat();
+const chat = getFakeChat(user);
+const mockChats = [chat, getFakeChat(), getFakeChat()];
+const mockRandomMessage = getFakeMessage;
 app.use('/', (req, res, next) => {
   req.user = user;
   next();
 });
 
-describe('POST message', () => {
+jest.mock('../models/queries', () => {
+  return {
+    getUserChats: (userId) => {
+      return mockChats.filter((chat) =>
+        chat.users.some((user) => user.id === userId)
+      );
+    },
+    createMessage: ({ chatId, userId, content, file }) => {
+      return mockRandomMessage({ content, file, userId, chatId });
+    },
+  };
+});
+
+describe.skip('POST message', () => {
   it('returns created message', () => {
     app.use('/', chatRouter);
     return request(app)
