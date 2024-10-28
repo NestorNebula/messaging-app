612db3d5a6bfb93f244626f5662aa0583d0eff52
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-28 17:54:16 +0100

    feat: add Input component

---
+++ client/src/components/input/Input.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+function Input({
+  name,
+  value,
+  update,
+  validation,
+  type = 'text',
+  label = name,
+}) {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        onChange={update}
+        value={value}
+      />
+      <span>{validation.isValid ? validation.message : null}</span>
+    </div>
+  );
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  update: PropTypes.func.isRequired,
+  validation: PropTypes.object.isRequired,
+  type: PropTypes.string,
+  label: PropTypes.string,
+};
+
+export default Input;
