7845982a6f657154a5f2e784f77f17629de7b5b1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-28 16:18:46 +0100

    feat: add Auth component and create auth route

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,9 +1,14 @@
 import App from '../App';
+import Auth from '../components/auth/Auth';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/auth',
+    element: <Auth />,
   },
 ];
 
