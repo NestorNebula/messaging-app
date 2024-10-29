b893c9d4ca4093860a0104c5d250b189fdd2a840
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-29 16:12:56 +0100

    test: add tests for login action

--- client/tests/Login.test.jsx
+++ client/tests/Login.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import routes from '../src/routes/routes';
 import { logInAction } from '../src/helpers/actions';
+import { asyncFetch } from '../src/helpers/fetch';
 
 beforeEach(() => {
   const router = createMemoryRouter(routes, {
@@ -13,12 +14,20 @@ beforeEach(() => {
 });
 
 vi.mock('../src/helpers/actions', { spy: true });
+vi.mock('../src/helpers/fetch', { spy: true });
 
 const typeWrongData = async (user) => {
   const usermailInput = screen.getByRole('textbox', { name: /username/i });
   await user.type(usermailInput, 'Username');
   const passwordInput = screen.getByLabelText(/password/i);
   await user.type(passwordInput, 'pwd');
+};
+
+const typeCorrectData = async (user) => {
+  const usermailInput = screen.getByRole('textbox', { name: /username/i });
+  await user.type(usermailInput, 'username');
+  const passwordInput = screen.getByLabelText(/password/i);
+  await user.type(passwordInput, 'password');
 };
 
 describe('Login Form', () => {
@@ -42,5 +51,40 @@ describe('Login Action', () => {
     await typeWrongData(user);
     await user.click(button);
     expect(logInAction).not.toHaveBeenCalled();
+  });
+
+  it('redirects to App route after successful login', async () => {
+    asyncFetch.mockImplementationOnce(() => {
+      return {
+        result: {
+          id: 1,
+        },
+        error: false,
+      };
+    });
+    const user = userEvent.setup();
+    await typeCorrectData(user);
+    const button = screen.getByRole('button', { name: /log in/i });
+    await user.click(button);
+    expect(screen.queryByRole('form', { name: /log in/i })).toBeNull();
+  });
+
+  it('renders login with errors after unsuccessful login', async () => {
+    asyncFetch.mockImplementationOnce(() => {
+      return {
+        result: {
+          error: {
+            message: 'Incorrect password.',
+          },
+        },
+        error: true,
+      };
+    });
+    const user = userEvent.setup();
+    await typeCorrectData(user);
+    const button = screen.getByRole('button', { name: /log in/i });
+    await user.click(button);
+    expect(screen.queryByRole('form', { name: /log in/i })).not.toBeNull();
+    expect(screen.queryByText(/incorrect password/i)).not.toBeNull();
   });
 });
