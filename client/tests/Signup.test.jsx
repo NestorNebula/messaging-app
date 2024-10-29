977cc2befc37804b509772400bcdb42cb817bc7d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-29 11:18:07 +0100

    test: add test for signup action

--- client/tests/Signup.test.jsx
+++ client/tests/Signup.test.jsx
@@ -1,8 +1,9 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import routes from '../src/routes/routes';
+import { signUpAction } from '../src/helpers/actions';
 
 beforeEach(() => {
   const router = createMemoryRouter(routes, {
@@ -11,6 +12,25 @@ beforeEach(() => {
   render(<RouterProvider router={router} />);
 });
 
+vi.mock('../src/helpers/actions', async () => {
+  const actual = await vi.importActual('../src/helpers/actions');
+  return {
+    ...actual,
+    signUpAction: vi.fn(() => {}),
+  };
+});
+
+const typeWrongData = async (user) => {
+  const usernameInput = screen.getByRole('textbox', { name: /username/i });
+  await user.type(usernameInput, 'thisisaverylongusernamethatcannotbeaccepted');
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  await user.type(emailInput, 'email');
+  const pwdInput = screen.getByLabelText('password');
+  await user.type(pwdInput, 'pwd');
+  const confirmInput = screen.getByLabelText(/confirm/i);
+  await user.type(confirmInput, 'pasword');
+};
+
 describe('Signup', () => {
   it('renders form', () => {
     expect(screen.queryByRole('form', { name: /sign up/i })).not.toBeNull();
@@ -18,20 +38,20 @@ describe('Signup', () => {
 
   it('displays error messages when typing incorrect values', async () => {
     const user = userEvent.setup();
-    const usernameInput = screen.getByRole('textbox', { name: /username/i });
-    await user.type(
-      usernameInput,
-      'thisisaverylongusernamethatcannotbeaccepted'
-    );
+    await typeWrongData(user);
     expect(screen.queryByText(/username must have/i)).not.toBeNull();
-    const emailInput = screen.getByRole('textbox', { name: /email/i });
-    await user.type(emailInput, 'email');
     expect(screen.queryByText(/email must have/i)).not.toBeNull();
-    const pwdInput = screen.getByLabelText('password');
-    await user.type(pwdInput, 'pwd');
-    const confirmInput = screen.getByLabelText(/confirm/i);
-    await user.type(confirmInput, 'pasword');
     expect(screen.queryAllByText(/password must have/i).length).toBe(2);
     expect(screen.queryByText(/passwords don't match/i)).not.toBeNull();
+  });
+
+  it("doesn't send form while data is incorrect", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: /sign up/i });
+    await user.click(button);
+    expect(signUpAction).not.toHaveBeenCalled();
+    await typeWrongData(user);
+    await user.click(button);
+    expect(signUpAction).not.toHaveBeenCalled();
   });
 });
