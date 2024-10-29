b015ecc0b2f8e14f87a7811f5b374af60625b290
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-29 09:48:18 +0100

    test: update tests setup
    Change API_URL env variable

--- client/tests/setup.js
+++ client/tests/setup.js
@@ -1,8 +1,12 @@
-import { afterEach, expect, vi } from 'vitest';
+import { afterEach, beforeAll, expect, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
+
+beforeAll(async () => {
+  vi.stubEnv('VITE_API_URL', 'http://localhost:4000');
+});
 
 afterEach(() => {
   cleanup();
