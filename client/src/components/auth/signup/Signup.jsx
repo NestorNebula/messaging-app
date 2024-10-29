e5f2f3743134817ec9acb1a5c49a0d096155d0a6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-29 11:19:54 +0100

    feat: add content to Signup component

--- client/src/components/auth/signup/Signup.jsx
+++ client/src/components/auth/signup/Signup.jsx
@@ -1,3 +1,70 @@
-function Signup() {}
+import { Form } from 'react-router-dom';
+import { useInput } from '../../../hooks/useInput';
+import Input from '../../input/Input';
+import {
+  validateUsername,
+  validateEmail,
+  validatePassword,
+  validatePasswordsMatch,
+} from '../../../helpers/inputValidation';
+
+function Signup() {
+  const {
+    value: username,
+    validation: usernameValidation,
+    updateValue: updateUsername,
+  } = useInput(validateUsername);
+  const {
+    value: email,
+    validation: emailValidation,
+    updateValue: updateEmail,
+  } = useInput(validateEmail);
+  const {
+    value: password,
+    validation: passwordValidation,
+    updateValue: updatePassword,
+  } = useInput(validatePassword);
+  const {
+    value: confirm,
+    validation: confirmValidation,
+    updateValue: updateConfirm,
+  } = useInput(validatePassword);
+  const passwordMatchValidation = validatePasswordsMatch(password, confirm);
+
+  return (
+    <Form method="post" aria-label="sign up">
+      <Input
+        name="username"
+        value={username}
+        update={updateUsername}
+        validation={usernameValidation}
+      />
+      <Input
+        name="email"
+        value={email}
+        update={updateEmail}
+        validation={emailValidation}
+        type="email"
+      />
+      <Input
+        name="password"
+        value={password}
+        update={updatePassword}
+        validation={passwordValidation}
+        type="password"
+      />
+      <Input
+        name="confirm"
+        value={confirm}
+        update={updateConfirm}
+        validation={confirmValidation}
+        type="password"
+        label="Confirm Password"
+      />
+      {passwordMatchValidation && <div>{passwordMatchValidation}</div>}
+      <button>Sign Up</button>
+    </Form>
+  );
+}
 
 export default Signup;
