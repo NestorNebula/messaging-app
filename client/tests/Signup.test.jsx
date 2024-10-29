1a0a74c369bce1a6343d9de3c260cacf418b26eb
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-29 10:16:47 +0100

    test: update Signup tests

--- client/tests/Signup.test.jsx
+++ client/tests/Signup.test.jsx
@@ -6,7 +6,7 @@ import routes from '../src/routes/routes';
 
 beforeEach(() => {
   const router = createMemoryRouter(routes, {
-    initialEntries: ['/auth/login'],
+    initialEntries: ['/auth/signup'],
   });
   render(<RouterProvider router={router} />);
 });
@@ -16,22 +16,22 @@ describe('Signup', () => {
     expect(screen.queryByRole('form', { name: /sign up/i })).not.toBeNull();
   });
 
-  it('display error message when typing incorrect values', async () => {
-    const user = await userEvent.setup();
+  it('displays error messages when typing incorrect values', async () => {
+    const user = userEvent.setup();
     const usernameInput = screen.getByRole('textbox', { name: /username/i });
     await user.type(
       usernameInput,
-      'thisisaverylongpasswordthatcannotbeaccepted'
+      'thisisaverylongusernamethatcannotbeaccepted'
     );
-    expect(screen.queryByText(/username should have/i)).not.toBeNull();
+    expect(screen.queryByText(/username must have/i)).not.toBeNull();
     const emailInput = screen.getByRole('textbox', { name: /email/i });
     await user.type(emailInput, 'email');
-    expect(screen.queryByText(/email should/i)).not.toBeNull();
-    const pwdInput = screen.getByRole('textbox', { name: /password/i });
+    expect(screen.queryByText(/email must have/i)).not.toBeNull();
+    const pwdInput = screen.getByLabelText('password');
     await user.type(pwdInput, 'pwd');
-    const confirmInput = screen.getByRole('textbox', { name: /confirm/i });
+    const confirmInput = screen.getByLabelText(/confirm/i);
     await user.type(confirmInput, 'pasword');
-    expect(screen.queryAllByText(/password should have/i).length).toBe(2);
+    expect(screen.queryAllByText(/password must have/i).length).toBe(2);
     expect(screen.queryByText(/passwords don't match/i)).not.toBeNull();
   });
 });
