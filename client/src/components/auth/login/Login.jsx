276eff23f300e848175f9fb57290cb964bbe5d29
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-29 15:42:18 +0100

    feat: add content to Login component

--- client/src/components/auth/login/Login.jsx
+++ client/src/components/auth/login/Login.jsx
@@ -1,3 +1,42 @@
-function Login() {}
+import { Form } from 'react-router-dom';
+import { useInput } from '../../../hooks/useInput';
+import Input from '../../input/Input';
+import {
+  validateUsermail,
+  validatePassword,
+} from '../../../helpers/inputValidation';
+
+function Login() {
+  const {
+    value: usermail,
+    validation: usermailValidation,
+    updateValue: updateUsermail,
+  } = useInput(validateUsermail);
+  const {
+    value: password,
+    validation: passwordValidation,
+    updateValue: updatePassword,
+  } = useInput(validatePassword);
+
+  return (
+    <Form method="post" aria-label="log in">
+      <Input
+        name="username"
+        value={usermail}
+        update={updateUsermail}
+        validation={usermailValidation}
+        label="Username or Email"
+      />
+      <Input
+        name="password"
+        value={password}
+        update={updatePassword}
+        validation={passwordValidation}
+        type="password"
+      />
+      <button>Log In</button>
+    </Form>
+  );
+}
 
 export default Login;
