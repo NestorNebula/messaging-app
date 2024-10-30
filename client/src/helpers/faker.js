8a46eac69a2e617d85149f5f402ddb74d6b27fb1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 12:03:18 +0100

    feat: add many getFakeData helper functions

--- client/src/helpers/faker.js
+++ client/src/helpers/faker.js
@@ -1,12 +1,50 @@
 import { faker } from '@faker-js/faker';
 
-const getFakeUser = () => {
+const getFakeUser = (id = faker.number.int({ max: 100 }), profile) => {
   const name = faker.person.firstName();
   return {
-    id: faker.number.int({ max: 100 }),
+    id,
     username: name,
     email: faker.internet.email({ firstName: name }),
+    profile: profile ? getFakeProfile(id, name) : null,
   };
 };
 
-export { getFakeUser };
+const getFakeProfile = (id = faker.number.int({ max: 100 }), name) => {
+  return {
+    displayName: name,
+    avatar: 'default.png',
+    bio: faker.person.bio(),
+    link: faker.internet.url(),
+    userId: id,
+  };
+};
+
+const getFakeChats = (userId) => {
+  return [getFakeChat(userId), getFakeChat(userId), getFakeChat(userId)];
+};
+
+const getFakeChat = (userId) => {
+  const secondUserId = faker.number.int({ max: 100 });
+  const chatId = faker.number.int({ max: 1000 });
+  return {
+    id: chatId,
+    creationDate: faker.date.past({ years: 1 }),
+    updatedAt: faker.date.recent(),
+    users: [getFakeUser(userId, true), getFakeUser(secondUserId, true)],
+    messages: [getFakeMessage(userId, chatId), getFakeMessage(userId, chatId)],
+  };
+};
+
+const getFakeMessage = (userId, chatId, file) => {
+  return {
+    id: faker.number.int({ max: 10000 }),
+    content: faker.lorem.paragraph(),
+    file: file ? 'default.png' : null,
+    creationDate: faker.date.recent(),
+    userId,
+    chatId,
+  };
+};
+
+export { getFakeUser, getFakeChats };
