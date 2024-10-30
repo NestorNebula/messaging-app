3c4d04fb6fb07b5236a3e2c7a41011d091fa88b2
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 11:13:56 +0100

    feat: add image in Messaging

--- client/src/components/messaging/Messaging.jsx
+++ client/src/components/messaging/Messaging.jsx
@@ -1,3 +1,11 @@
-function Messaging() {}
+function Messaging() {
+  return (
+    <main>
+      <header>
+        <img src="" alt="messages page" />
+      </header>
+    </main>
+  );
+}
 
 export default Messaging;
