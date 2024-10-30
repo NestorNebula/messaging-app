09f620d267fd348431e030fc9e3c99be0294924f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 16:56:15 +0100

    feat: add Messaging sidebar content

--- client/src/components/messaging/Messaging.jsx
+++ client/src/components/messaging/Messaging.jsx
@@ -1,4 +1,21 @@
+import { useContext } from 'react';
+import { MessagingContext } from '../../context/MessagingContext';
+import { useData } from '../../hooks/useData';
+import Error from '../elements/Error';
+import Loading from '../elements/Loading';
+
 function Messaging() {
+  const { user } = useContext(MessagingContext);
+  const {
+    data: chats,
+    error,
+    loading,
+  } = useData(`users/${user.id}/chats`, { method: 'get' }, []);
+  const sortChats = (a, b) => {
+    a.updatedAt >= b.updatedAt ? -1 : 1;
+  };
+
+  const checkIdMatch = (id) => id === user.id;
   return (
     <main>
       <header>
@@ -6,7 +23,36 @@ function Messaging() {
       </header>
       <section>
         <div>Messages</div>
-        <div></div>
+        <div>
+          {error ? (
+            <Error error={error} />
+          ) : loading ? (
+            <Loading contentName="messages" />
+          ) : (
+            chats.sort(sortChats) &&
+            chats.map((chat) => (
+              <div key={chat.id}>
+                <div>
+                  {chat.users.map((usr) => {
+                    return checkIdMatch(usr.id) ? null : (
+                      <img
+                        src={`avatars/${usr.avatar}`}
+                        alt={`${usr.username}'s avatar`}
+                      />
+                    );
+                  })}
+                </div>
+                <div>
+                  {chat.users.map((usr) => {
+                    return checkIdMatch(usr.id) ? null : (
+                      <span>{usr.profile.displayName}</span>
+                    );
+                  })}
+                </div>
+              </div>
+            ))
+          )}
+        </div>
       </section>
     </main>
   );
