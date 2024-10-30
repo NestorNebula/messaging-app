7be4b123dfab0de114c496f776c87e31052fcaf4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 18:24:54 +0100

    feat: add chat expended section basis
    Currently displaying messages content of the actual chat

--- client/src/components/messaging/Messaging.jsx
+++ client/src/components/messaging/Messaging.jsx
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { MessagingContext } from '../../context/MessagingContext';
 import { useData } from '../../hooks/useData';
+import { sortMessages } from '../../helpers/messagingUtils';
 import Sidebar from './sidebar/Sidebar';
 
 function Messaging() {
@@ -10,13 +11,44 @@ function Messaging() {
     error,
     loading,
   } = useData(`users/${user.id}/chats`, { method: 'get' }, []);
-
+  const [actualChat, setActualChat] = useState(0);
+  const updateActualChat = (newActualChat) => {
+    setActualChat(newActualChat.id);
+  };
   return (
     <main>
       <header>
         <img src="" alt="messages page" />
       </header>
-      <Sidebar chats={chats} error={error} loading={loading} userId={user.id} />
+      <Sidebar
+        chats={chats}
+        error={error}
+        loading={loading}
+        userId={user.id}
+        updateActualChat={updateActualChat}
+      />
+      <section>
+        {chats ? (
+          <>
+            <h5>
+              {chats[actualChat].users.map((usr) => (
+                <span key={`${usr.username}title`}>{usr.username}</span>
+              ))}
+            </h5>
+            <div>
+              {chats[actualChat].messages.sort(sortMessages) &&
+                chats[actualChat].messages.map((message) => (
+                  <div key={`${message.id}content`}>
+                    <div>{message.content}</div>
+                    <div>{Date(message.creationDate)}</div>
+                  </div>
+                ))}
+            </div>
+          </>
+        ) : (
+          <div>No messages to display.</div>
+        )}
+      </section>
     </main>
   );
 }
