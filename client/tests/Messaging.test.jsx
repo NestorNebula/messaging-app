627fac4da23a67452975b4bc81cb873b2d897222
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 15:47:01 +0100

    feat: add tests for Messaging

--- client/tests/Messaging.test.jsx
+++ client/tests/Messaging.test.jsx
@@ -2,7 +2,11 @@ import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { screen, render } from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import routes from '../src/routes/routes';
-import { getFakeUser } from '../src/helpers/faker';
+import { getFakeUser, getFakeChats } from '../src/helpers/faker';
+import { useData } from '../src/hooks/useData';
+
+const mockUser = getFakeUser();
+const mockChats = getFakeChats();
 
 beforeEach(async () => {
   const router = createMemoryRouter(routes, {
@@ -17,13 +21,40 @@ vi.mock('../src/helpers/loaders', async () => {
   return {
     ...actual,
     appLoader: () => {
-      return { user: getFakeUser() };
+      return { user: mockUser };
     },
   };
 });
+vi.mock('../src/hooks/useData', { spy: true });
+useData.mockImplementation(() => {
+  return { data: mockChats, error: null, loading: false };
+});
+
+const getRecentChat = (chats) => {
+  const recent = chats.reduce(
+    (value, current) =>
+      (value = current.creationDate > value.creationDate ? current : value),
+    chats[0]
+  );
+  return recent;
+};
 
 describe('Messaging', () => {
   it('renders Messaging as App index route', () => {
     expect(screen.queryByText(/messages/i)).not.toBeNull();
+  });
+
+  it('renders all chats', () => {
+    expect(screen.queryAllByAltText(/avatar/i).length).toBe(mockChats.length);
+  });
+
+  it('renders chats in order', () => {
+    const recent = getRecentChat(mockChats);
+    expect(screen.queryAllByAltText(/avatar/i)[0].alt).toMatch(recent.users[1]);
+  });
+
+  it('renders first chat messages', () => {
+    const recent = getRecentChat(mockChats);
+    expect(screen.queryByText(recent.messages[0])).not.toBeNull();
   });
 });
