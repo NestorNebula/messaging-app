a39a32409f1d6a69f4a95a49c0e74092eef37f68
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 09:05:08 +0100

    feat: add app loader

--- client/src/helpers/loaders.js
+++ client/src/helpers/loaders.js
@@ -1,4 +1,32 @@
 import { redirect } from 'react-router-dom';
+import { asyncFetch } from './fetch';
+
+const appLoader = async () => {
+  const userId = localStorage.getItem('id');
+  if (!userId) return redirect('/auth/login');
+  let user = null;
+  let count = 0;
+  while (count < 2) {
+    const fetch = await asyncFetch({ path: `users/${userId}`, method: 'get' });
+    if (fetch.error) {
+      const refreshFetch = await asyncFetch({
+        path: 'auth/refresh',
+        method: 'get',
+      });
+      if (refreshFetch.error) {
+        localStorage.removeItem('id');
+        return redirect('/auth/login');
+      }
+      localStorage.setItem('id', refreshFetch.result.id);
+    } else {
+      user = fetch.result;
+    }
+    if (user) break;
+    count++;
+  }
+  if (!user) return redirect('/auth/login');
+  return { user };
+};
 
 const authLoader = () => {
   const userId = localStorage.getItem('id');
@@ -6,4 +34,4 @@ const authLoader = () => {
   return null;
 };
 
-export { authLoader };
+export { appLoader, authLoader };
