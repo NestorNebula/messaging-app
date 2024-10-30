b98b48cf070802c4f0f9ceb1597e7831cf705ad4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 09:27:33 +0100

    feat: add MessagingContext and Outlet in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import { Outlet, useLoaderData } from 'react-router-dom';
+import { MessagingContext } from './context/MessagingContext';
 import './App.css';
 
 function App() {
-  return <></>;
+  const { user } = useLoaderData();
+  return (
+    <MessagingContext.Provider value={{ user }}>
+      <Outlet />
+    </MessagingContext.Provider>
+  );
 }
 
 export default App;
