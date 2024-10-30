030fbf25939afb39f09a8c2807cc4a924a9801a7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-30 18:24:26 +0100

    feat: add sortMessages helper function

--- client/src/helpers/messagingUtils.js
+++ client/src/helpers/messagingUtils.js
@@ -2,6 +2,10 @@ const sortChats = (a, b) => {
   a.updatedAt >= b.updatedAt ? -1 : 1;
 };
 
+const sortMessages = (a, b) => {
+  a.creationDate >= b.creationDate ? 1 : -1;
+};
+
 const checkIdMatch = (id, userId) => id === userId;
 
-export { sortChats, checkIdMatch };
+export { sortChats, sortMessages, checkIdMatch };
