2515c7ed38af2cdd973849d762f5a4afaff9de73
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-31 15:03:59 +0100

    feat: add Messaging to App route

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -2,14 +2,22 @@ import App from '../App';
 import Auth from '../components/auth/Auth';
 import Signup from '../components/auth/signup/Signup';
 import Login from '../components/auth/login/Login';
+import Messaging from '../components/messaging/Messaging';
 import { appLoader, authLoader } from '../helpers/loaders';
-import { signUpAction, logInAction } from '../helpers/actions';
+import { signUpAction, logInAction, messagingAction } from '../helpers/actions';
 
 const routes = [
   {
     path: '/',
     element: <App />,
     loader: appLoader,
+    children: [
+      {
+        index: true,
+        element: <Messaging />,
+        action: messagingAction,
+      },
+    ],
   },
   {
     path: '/auth',
