509f63b34b03729b2f3848e69a2412c058d70535
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-31 18:09:48 +0100

    feat: add account's profile infos in Account

--- client/src/components/account/Account.jsx
+++ client/src/components/account/Account.jsx
@@ -1,3 +1,34 @@
-function Account() {}
+import { useContext } from 'react';
+import { MessagingContext } from '../../context/MessagingContext';
+import { useData } from '../../hooks/useData';
+import Error from '../elements/Error';
+import Loading from '../elements/Loading';
+
+function Account() {
+  const { user } = useContext(MessagingContext);
+  const { data: profile, error, loading } = useData(`users/${user.id}/profile`);
+  return (
+    <>
+      <header>
+        <button aria-label="settings">
+          <img src="" alt="" />
+        </button>
+      </header>
+      {error ? (
+        <Error error={error} />
+      ) : loading ? (
+        <Loading contentName="account" />
+      ) : (
+        <section>
+          <img src={`avatars/${profile.avatar}`} alt="" />
+          <div>@{profile.user.username}</div>
+          <div>{profile.displayName}</div>
+          <div>{profile.bio}</div>
+          <div>{profile.link}</div>
+        </section>
+      )}
+    </>
+  );
+}
 
 export default Account;
