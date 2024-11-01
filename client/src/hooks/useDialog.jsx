aac07580c06d340b67847fb70d18a55c29a67724
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-01 09:04:52 +0100

    feat: add useDialog hook

---
+++ client/src/hooks/useDialog.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useDialog = () => {
+  const [open, setOpen] = useState(false);
+  const dialogRef = useRef();
+  useEffect(() => {
+    if (open) dialogRef.current.showModal();
+    else dialogRef.current.close();
+  }, [open]);
+
+  return { dialogRef, setOpen };
+};
+
+export { useDialog };
