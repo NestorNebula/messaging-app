b25025a316fc40648773df7200909a3f183a0735
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-01 14:45:05 +0100

    feat: add forms in Account component

--- client/src/components/account/Account.jsx
+++ client/src/components/account/Account.jsx
@@ -1,32 +1,58 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { MessagingContext } from '../../context/MessagingContext';
 import { useData } from '../../hooks/useData';
 import { useDialog } from '../../hooks/useDialog';
 import Error from '../elements/Error';
 import Loading from '../elements/Loading';
 import Profile from '../profile/Profile';
+import InformationsForm from './forms/InformationsForm';
+import ProfileForm from './forms/ProfileForm';
 
 function Account() {
   const { user } = useContext(MessagingContext);
   const { data: profile, error, loading } = useData(`users/${user.id}/profile`);
   const { dialogRef, setOpen } = useDialog();
+  const [displayForm, setDisplayForm] = useState(false);
   return (
     <>
-      <header>
-        <button onClick={() => setOpen(true)} aria-label="settings">
-          <img src="" alt="" />
-        </button>
-      </header>
-      <dialog onCancel={() => setOpen(false)} ref={dialogRef}>
-        <button>Update Private Informations</button>
-        <button>Update Profile</button>
-      </dialog>
-      {error ? (
-        <Error error={error} />
-      ) : loading ? (
-        <Loading contentName="account" />
+      {displayForm ? (
+        (
+          <header>
+            <button
+              onClick={() => setDisplayForm(false)}
+              aria-label="close form"
+            >
+              <img src="" alt="" />
+            </button>
+          </header>
+        ) && displayForm === 'informations' ? (
+          <InformationsForm user={user} />
+        ) : (
+          <ProfileForm profile={profile} />
+        )
       ) : (
-        <Profile profile={profile} />
+        <>
+          <header>
+            <button onClick={() => setOpen(true)} aria-label="settings">
+              <img src="" alt="" />
+            </button>
+          </header>
+          <dialog onCancel={() => setOpen(false)} ref={dialogRef}>
+            <button onClick={() => setDisplayForm('informations')}>
+              Update Private Informations
+            </button>
+            <button onClick={() => setDisplayForm('profile')}>
+              Update Profile
+            </button>
+          </dialog>
+          {error ? (
+            <Error error={error} />
+          ) : loading ? (
+            <Loading contentName="account" />
+          ) : (
+            <Profile profile={profile} />
+          )}
+        </>
       )}
     </>
   );
