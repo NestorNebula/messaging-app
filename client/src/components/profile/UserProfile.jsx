9293a40286dca35ecfa3b52d30c5d07b7dadb6c6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-02 11:05:53 +0100

    feat: add content to UserProfile

--- client/src/components/profile/UserProfile.jsx
+++ client/src/components/profile/UserProfile.jsx
@@ -1,3 +1,33 @@
-function UserProfile() {}
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { MessagingContext } from '../../context/MessagingContext';
+import { useData } from '../../hooks/useData';
+import Error from '../elements/Error';
+import Loading from '../elements/Loading';
+import Profile from './Profile';
+
+function UserProfile() {
+  const { user } = useContext(MessagingContext);
+  const { userId } = useParams();
+  const {
+    data: profile,
+    error,
+    loading,
+  } = useData(`users/${userId}/profile`, { method: 'get' });
+
+  return (
+    <section>
+      {error ? (
+        <Error error={error} />
+      ) : loading ? (
+        <Loading contentName="user profile" />
+      ) : (
+        <>
+          <Profile profile={profile} />
+        </>
+      )}
+    </section>
+  );
+}
 
 export default UserProfile;
