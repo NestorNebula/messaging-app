35e41d89baf55ba8de8e91b8447b7951239fdfa6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-02 12:01:55 +0100

    feat: add Forms for updating friendship status

--- client/src/components/profile/UserProfile.jsx
+++ client/src/components/profile/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { Form, useActionData, useParams } from 'react-router-dom';
 import { MessagingContext } from '../../context/MessagingContext';
 import { useData } from '../../hooks/useData';
 import Error from '../elements/Error';
@@ -9,11 +9,12 @@ import Profile from './Profile';
 function UserProfile() {
   const { user } = useContext(MessagingContext);
   const { userId } = useParams();
+  const result = useActionData();
   const {
     data: profile,
     error,
     loading,
-  } = useData(`users/${userId}/profile`, { method: 'get' });
+  } = useData(`users/${userId}/profile`, { method: 'get' }, [result]);
 
   return (
     <section>
@@ -23,6 +24,26 @@ function UserProfile() {
         <Loading contentName="user profile" />
       ) : (
         <>
+          {result && result.error && <div>{result.error.message}</div>}
+          <div>
+            {profile.user.friends.some((friend) => friend.id === user.id) ? (
+              <Form method="put">
+                <input type="hidden" name="userId" value={user.id} />
+                <input type="hidden" name="friendId" value={profile.userId} />
+                <button name="intent" value="remove-friend">
+                  Remove Friend
+                </button>
+              </Form>
+            ) : (
+              <Form method="put">
+                <input type="hidden" name="userId" value={user.id} />
+                <input type="hidden" name="friendId" value={profile.userId} />
+                <button name="intent" value="add-friend">
+                  Add Friend
+                </button>
+              </Form>
+            )}
+          </div>
           <Profile profile={profile} />
         </>
       )}
