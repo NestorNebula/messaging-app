67cf01ed279e6fd02d2c19e78db23fd77d85abb0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-02 10:00:16 +0100

    test: add test for onlyFriends set to true

--- client/tests/UsersList.test.jsx
+++ client/tests/UsersList.test.jsx
@@ -3,10 +3,15 @@ import { render, screen } from '@testing-library/react';
 import { useData } from '../src/hooks/useData';
 import { MemoryRouter } from 'react-router-dom';
 import UsersList from '../src/components/usersList/UsersList';
-import { getFakeProfile, getFakeUser } from '../src/helpers/faker';
+import {
+  getFakeFriends,
+  getFakeProfile,
+  getFakeUser,
+} from '../src/helpers/faker';
 
 const mockUser = getFakeUser();
 const mockUsers = [getFakeProfile(), getFakeProfile(), getFakeProfile()];
+const mockFriends = getFakeFriends();
 
 vi.mock('react', async () => {
   const actual = await vi.importActual('react');
@@ -21,7 +26,7 @@ vi.mock('../src/hooks/useData', { spy: true });
 useData.mockImplementation((path) => {
   return path === `profiles`
     ? { data: mockUsers, error: null, loading: false }
-    : { data: [], error: null, loading: false };
+    : { data: mockFriends, error: null, loading: false };
 });
 
 describe('Users List', () => {
@@ -34,5 +39,24 @@ describe('Users List', () => {
     expect(screen.queryByText(mockUsers[0].displayName)).not.toBeNull();
     expect(screen.queryByText(mockUsers[1].displayName)).not.toBeNull();
     expect(screen.queryByText(mockUsers[2].displayName)).not.toBeNull();
+  });
+});
+
+describe('Users List, Only Friends', () => {
+  it("renders all user's friends", () => {
+    render(
+      <MemoryRouter>
+        <UsersList onlyFriends={true} />
+      </MemoryRouter>
+    );
+    expect(
+      screen.queryByText(mockFriends[0].profile.displayName)
+    ).not.toBeNull();
+    expect(
+      screen.queryByText(mockFriends[1].profile.displayName)
+    ).not.toBeNull();
+    expect(
+      screen.queryByText(mockFriends[2].profile.displayName)
+    ).not.toBeNull();
   });
 });
