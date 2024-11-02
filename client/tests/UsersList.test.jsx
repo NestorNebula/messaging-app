fc7ef7949211a907ce60266de36c1ebd0b9088ba
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-02 09:40:23 +0100

    test: add missing mocks to UsersList test file

--- client/tests/UsersList.test.jsx
+++ client/tests/UsersList.test.jsx
@@ -1,11 +1,22 @@
 import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { useData } from '../src/hooks/useData';
+import { MemoryRouter } from 'react-router-dom';
 import UsersList from '../src/components/usersList/UsersList';
-import { getFakeProfile } from '../src/helpers/faker';
+import { getFakeProfile, getFakeUser } from '../src/helpers/faker';
 
+const mockUser = getFakeUser();
 const mockUsers = [getFakeProfile(), getFakeProfile(), getFakeProfile()];
 
+vi.mock('react', async () => {
+  const actual = await vi.importActual('react');
+  return {
+    ...actual,
+    useContext: () => {
+      return { user: mockUser };
+    },
+  };
+});
 vi.mock('../src/hooks/useData', { spy: true });
 useData.mockImplementation((path) => {
   return path === `profiles`
@@ -15,7 +26,11 @@ useData.mockImplementation((path) => {
 
 describe('Users List', () => {
   it('renders all users', () => {
-    render(<UsersList onlyFriends={false} />);
+    render(
+      <MemoryRouter>
+        <UsersList onlyFriends={false} />
+      </MemoryRouter>
+    );
     expect(screen.queryByText(mockUsers[0].displayName)).not.toBeNull();
     expect(screen.queryByText(mockUsers[1].displayName)).not.toBeNull();
     expect(screen.queryByText(mockUsers[2].displayName)).not.toBeNull();
