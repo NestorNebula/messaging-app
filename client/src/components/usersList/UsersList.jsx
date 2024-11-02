96930052e222f169e2b0ab9117d47e7756e17b22
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-02 10:00:55 +0100

    feat: add content to UsersList

--- client/src/components/usersList/UsersList.jsx
+++ client/src/components/usersList/UsersList.jsx
@@ -1,3 +1,63 @@
-function UsersList() {}
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { MessagingContext } from '../../context/MessagingContext';
+import { useData } from '../../hooks/useData';
+import Error from '../elements/Error';
+import Loading from '../elements/Loading';
+import PropTypes from 'prop-types';
+
+function UsersList({ onlyFriends }) {
+  const { user } = useContext(MessagingContext);
+  const {
+    data: users,
+    error,
+    loading,
+  } = useData(onlyFriends ? `users/${user.id}/friends` : 'profiles', {
+    method: 'get',
+  });
+
+  return (
+    <section>
+      {error ? (
+        <Error error={error} />
+      ) : loading ? (
+        <Loading contentName={onlyFriends ? 'friends' : 'users'} />
+      ) : (
+        <>
+          <div>{onlyFriends ? 'Your friends' : 'Popular users'}</div>
+          <div>
+            {users.map((person) =>
+              onlyFriends ? (
+                <div key={person.id}>
+                  <Link to={`profile/${person.id}`}>
+                    <img src={`avatars/${person.profile.avatar}`} alt="" />
+                  </Link>
+                  <div>
+                    <div>{person.profile.displayName}</div>
+                    <div>(@{person.username})</div>
+                  </div>
+                </div>
+              ) : (
+                <div key={person.userId}>
+                  <Link to={`profile/${person.userId}`}>
+                    <img src={`avatars/${person.avatar}`} alt="" />
+                  </Link>
+                  <div>
+                    <div>{person.displayName}</div>
+                    <div>(@{person.user.username})</div>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </>
+      )}
+    </section>
+  );
+}
+
+UsersList.propTypes = {
+  onlyFriends: PropTypes.bool,
+};
 
 export default UsersList;
