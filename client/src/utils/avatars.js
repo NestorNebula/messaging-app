3db88dca746366bfdd690f25e8910422cc7fc689
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-04 16:02:19 +0100

    feat: add app's avatars

--- client/src/utils/avatars.js
+++ client/src/utils/avatars.js
@@ -3,6 +3,42 @@ const avatars = [
     file: 'default.png',
     label: 'default avatar',
   },
+  {
+    file: 'penguin.png',
+    label: 'a penguin',
+  },
+  {
+    file: 'flamingo.png',
+    label: 'a flamingo buoy',
+  },
+  {
+    file: 'controller.png',
+    label: 'a controller',
+  },
+  {
+    file: 'cooking.png',
+    label: 'a frying pan with ingredients',
+  },
+  {
+    file: 'basketball.png',
+    label: 'a basketball',
+  },
+  {
+    file: 'happymonster.png',
+    label: 'a green monster smiling',
+  },
+  {
+    file: 'notsohappymonster.png',
+    label: 'a red monster annoyed',
+  },
+  {
+    file: 'rocket.png',
+    label: 'a rocket',
+  },
+  {
+    file: 'clapperboard.png',
+    label: 'a clapperboard',
+  },
 ];
 
 export default avatars;
