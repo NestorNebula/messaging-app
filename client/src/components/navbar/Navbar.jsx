0ae181a11fdd913dd72fbfcaa88e28be9bc787ca
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-06 09:34:50 +0100

    feat: add styles to Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,14 +1,29 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styles from './Navbar.module.css';
 
 function Navbar({ user }) {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
-        <li>{user ? <Link to="/">Home</Link> : 'Messaging App'}</li>
         <li>
           {user ? (
-            <Link to="/account">Account</Link>
+            <Link to="/">
+              <img src="icons/chat.svg" alt="chats" className={styles.icon} />
+            </Link>
+          ) : (
+            'Messaging App'
+          )}
+        </li>
+        <li>
+          {user ? (
+            <Link to="/account">
+              <img
+                src="icons/account.svg"
+                alt="account"
+                className={styles.icon}
+              />
+            </Link>
           ) : (
             'Log In to start sending messages'
           )}
