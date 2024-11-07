71cfcdee9a79f8e8d2cecd6017669f3c04044d22
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-07 09:50:50 +0100

    feat: add styles to ErrorElement

--- client/src/components/elements/ErrorElement.jsx
+++ client/src/components/elements/ErrorElement.jsx
@@ -1,13 +1,12 @@
 import { Link, useRouteError } from 'react-router-dom';
+import styles from './ErrorElement.module.css';
 
 function ErrorElement() {
   const error = useRouteError();
   return (
-    <section>
-      <div>
-        <div>{error.message || error.statusText}</div>
-        <div>{error.status}</div>
-      </div>
+    <section className={styles.error}>
+      <div>{error.message || error.statusText}</div>
+      <div>{error.status}</div>
       {error.status === 404 && <Link to="/">Return to Homepage</Link>}
     </section>
   );
